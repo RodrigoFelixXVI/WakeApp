0d26d4079ec32a8dd70dea4556b8792bf4ab4d36
Felix <60907942+RodrigoFelixXVI@users.noreply.github.com>
2020-12-06 21:16:23 -0600

    Add files via upload

---
+++ eliminar_alarma.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'conexion.php';
+
+$fecha = filter_input(INPUT_POST, "Fecha");
+
+$nombre = filter_input(INPUT_POST, "nombre");
+
+$telefono = filter_input(INPUT_POST, "tel");
+
+
+
+$query = "DELETE FROM alarma WHERE Nombre = '".$nombre."' AND  Hora = '".$fecha."'";
+
+$ejecutar = mysqli_query($conexion, $query);
+
+$resultado = mysqli_fetch_array($ejecutar);
+
+if($ejecutar){
+	echo 1;
+}
+
+?>

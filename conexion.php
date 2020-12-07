f106db80d80cfe1524f0cf71caa02a89489b3822
Felix <60907942+RodrigoFelixXVI@users.noreply.github.com>
2020-12-06 19:17:13 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <? php
-$host = "localhost";
-$usuario = "root";
-$contra = "";
-$db = "wakeappcrud";
+$host = "us-cdbr-east-02.cleardb.com";
+$usuario = "b9a1666b8ae3b3";
+$contra = "007eba1d";
+$db = "heroku_79e42d8ee832be6";
 
 $conexion = new mysqli($host, $usuario, $contra, $db);
 if($conexion->connect_error){
